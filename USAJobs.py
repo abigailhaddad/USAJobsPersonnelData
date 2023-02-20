b0e426671de20b4d5788f5f0a9709b84ebe6eb1f
Abigail Haddad <abigail.haddad@gmail.com>
2023-02-20 15:56:02 -0500

    -page numbering starts on 0 not 1 -adding searching by job title for current search -adding historical search

--- USAJobs.py
+++ USAJobs.py
@@ -15,10 +15,25 @@ def connect(authorization_key):
  'User-Agent': 'abigail.haddad@gmail.com'}
     return headers
 
-def current_search(authorization_key, keyword):
+    
+def historical_search(authorization_key):
     #formats url and makes request to API
-    number=str(1) 
-    base_url=f"https://data.usajobs.gov/api/search?Keyword={keyword}&?WhoMayApply=All&p={number}"
+    number=str(0) 
+    base_url=f"https://data.usajobs.gov/api/historicjoa?PositionSeries=2210&PageNumber={number}"
+    results = requests.get(base_url, headers=connect(authorization_key)).json()
+    searchResultDF= pd.DataFrame.from_dict(results['data'])
+    if len(searchResultDF) == 25:
+            while results['data']!= 0:
+                number=str(int(number)+1)      
+                results = requests.get(base_url+number, headers=connect(authorization_key)).json()
+                newDF=pd.DataFrame.from_dict(results['data'])
+                searchResultDF=pd.concat([searchResultDF, newDF])
+    return(searchResultDF)
+
+def current_search(authorization_key, keyword="", positiontitle=""):
+    #formats url and makes request to API
+    number=str(0) 
+    base_url=f"https://data.usajobs.gov/api/search?PositionTitle={positiontitle}&Keyword={keyword}&?WhoMayApply=All&p={number}"
     results = requests.get(base_url, headers=connect(authorization_key)).json()
     searchResultDF= pd.DataFrame.from_dict(results['SearchResult']['SearchResultItems'])
     if len(searchResultDF) == 25:
@@ -54,12 +69,21 @@ def getLogin(directory):
     authorization_key = open(directory.replace("code","key/authorization_key.txt"), "r").read()
     return(authorization_key)
 
-def main(keyword):
+def main(keyword, positiontitle):
     directory = os.getcwd()
     authorization_key=getLogin(directory)
-    df=current_search(authorization_key, keyword)
+    df=current_search(authorization_key, keyword, positiontitle)
     dfExtended=pullFieldsFromDict(df)
     dfExtended.to_excel(os.getcwd().replace("code", "data\currentResults.xlsx"))
     return(dfExtended)
 
-df=main("data")
+keyword=""
+positiontitle="data scientist"
+df=main(keyword, positiontitle)
+# 2,475 seems like the max results. 
+"""
+directory = os.getcwd()
+authorization_key=getLogin(directory)
+df_old=historical_search(authorization_key)
+dfExtended=pullFieldsFromDict(df_old)
+"""
