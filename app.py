6ec287a0376782fece9e7f6f541812398dbb6235
Abigail Haddad <abigail.haddad@gmail.com>
2023-04-16 15:25:40 +0000

    -app has filters but they're not working yet

--- app.py
+++ app.py
@@ -13,15 +13,60 @@ import dash_html_components as html
 import pandas as pd
 
 # Load your DataFrame
-df = pd.read_csv("/home/abigailhaddad1/projects/USAJobs/data/selectedCols.csv")
+df = pd.read_csv("/home/abigailhaddad1/projects/USAJobs/data/selected_cols_cleaned.csv")
+df["Close Date"]=pd.to_datetime(df['Close Date'])
 
 external_stylesheets = [dbc.themes.BOOTSTRAP]
 
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets, url_base_pathname='/usajobs/')
 server = app.server  # Add this line
 
+def generate_tooltip_data(df):
+    tooltip_data = []
+
+    for i in range(len(df)):
+        tooltip_row = {}
+        for col in df.columns:
+            tooltip_row[col] = {'value': df.at[i, col], 'type': 'markdown'}
+        tooltip_data.append(tooltip_row)
+
+    return tooltip_data
+
+tooltip_data = generate_tooltip_data(df)
+
+
 app.layout = html.Div([
-    html.H1('Selection of USAJobs Listings'),
+    html.H1('Selection of USAJobs Listings With Data Science Tasks'),
+    html.Div([
+        html.Label('Filter by Salary Range:'),
+        dcc.RangeSlider(
+            id='salary-range-slider',
+            min=0,
+            max=200000,
+            step=1000,
+            value=[30000, 100000],
+            marks={i: f'${i:,}' for i in range(0, 200001, 20000)},
+        ),
+        html.Div(id='slider-output-container')
+    ]),
+    html.Div([
+        html.Label('Filter by Close Date Range:'),
+        dcc.RangeSlider(
+            id='date-range-slider',
+            min=int(df['Close Date'].min().timestamp()),
+            max=int(df['Close Date'].max().timestamp()),
+            step=86400,  # One day step size
+            value=[
+                int(df['Close Date'].min().timestamp()),
+                int(df['Close Date'].max().timestamp())
+            ],
+            marks={
+                int(ts.timestamp()): ts.strftime('%Y-%m-%d')
+                for ts in pd.date_range(df['Close Date'].min(), df['Close Date'].max(), freq='1M')
+            },
+        ),
+        html.Div(id='date-slider-output-container')
+    ]),
     dash_table.DataTable(
         column_selectable='multi',
         id='table',
@@ -29,7 +74,14 @@ app.layout = html.Div([
         data=df.to_dict('records'),
         filter_action="native",  # Enables filtering
         sort_action="native",    # Enables sorting
-        style_cell={'whiteSpace': 'normal', 'height': 'auto'},  # Fixes cell height and wrap text
+        style_cell={
+            'whiteSpace': 'nowrap',
+            'overflow': 'hidden',
+            'textOverflow': 'ellipsis',
+        },
+        style_data={
+            'maxWidth': '150px',  # Adjust this value to control the column width
+        },
         style_data_conditional=[  # Adjusts font size
             {
                 'if': {'row_index': 'odd'},
@@ -42,8 +94,12 @@ app.layout = html.Div([
         },
         export_format='csv',  # Enables CSV export
         export_headers='display',  # Uses displayed header names in the exported file
+        tooltip_data=tooltip_data,
+        tooltip_duration=None,  # Set this property to keep the tooltip open indefinitely
     ),
 ])
+
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
